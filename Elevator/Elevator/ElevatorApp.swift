1b1793242cea9b3b1b2769c20c010ba6367f09c0
DomenicoFr <108270655+DomenicoFr@users.noreply.github.com>
2022-06-28 16:50:01 +0200

    Add files via upload

---
+++ Elevator/Elevator/ElevatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorApp.swift
+//  Elevator
+//
+//  Created by Dominik Franka on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ElevatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
